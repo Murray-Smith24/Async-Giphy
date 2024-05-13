6717d20eb77e469b379dacb6708bd46fe5375828
Murray-Smith24 <Murray.Smith@southwark.gov.uk>
2024-05-13 15:27:38 +0100

    added code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 require('dotenv').config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.API_KEY) // Print out value of API key stored in .env file
