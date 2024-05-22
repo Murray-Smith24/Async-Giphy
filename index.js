534c60853bbeecd9b486904e38c5b76feacbce17
Murray-Smith24 <Murray.Smith@southwark.gov.uk>
2024-05-22 10:42:35 +0100

    added code

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 require('dotenv').config();
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
 
-console.log(process.env.API_KEY) // Print out value of API key stored in .env file
+async function getImage(query) {
+    const temp = await fetch(endpoint + "&q=" + query)
+    const data = await temp.json()
+    const gifUrl = data.data[0].url
+    console.log("Gif Url: " + gifUrl)
+    return gifUrl
+}
+
+getImage("dogs");
+console.log(process.env.API_KEY) // Print out value of API key stored in .env file 
